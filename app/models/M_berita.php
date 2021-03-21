a14ce03ccb41631211b2c515cb5b1a4b936261ea
irvandandung <irvandandung1@gmail.com>
2021-03-21 19:55:24 +0700

    update model berita & karyawan

---
+++ app/models/M_berita.php
@@ -0,0 +1,38 @@
+<?php
+
+class M_berita {
+    private $primaryTable = 'berita';
+    private $joinTableKategoriBerita = 'kategori_berita';
+    private $db;
+
+    function __construct() {
+        $this->db = new Database();
+    }
+
+    //function for get all data berita from tabel berita in db
+    public function getAllBerita(){
+        $query = 'SELECT * from '.$this->primaryTable.' a 
+                LEFT JOIN '.$this->joinTableKategoriBerita.' b 
+                ON a.id_kategori_jabatan = b.id';
+        $this->db->query($query);
+        return $this->db->results();
+    }
+
+    //function for get data berita by id from tabel berita in db
+    public function getBerita($id){
+        $query = 'SELECT * from '.$this->primaryTable.' a 
+            LEFT JOIN '.$this->joinTableKategoriBerita.' b 
+            ON a.id_kategori_jabatan = b.id
+            WHERE a.id =:id';
+        $this->db->query($query);
+        $this->db->bind('id', $id);
+        return $this->db->result();
+    }
+
+    //function for get all data kategori berita from tabel berita in db
+    public function getAllKategoriBerita(){
+        $query = 'SELECT * from '.$this->joinTableKategoriBerita;
+        $this->db->query($query);
+        return $this->db->results();
+    }
+}
--- app/models/M_karyawan.php
+++ app/models/M_karyawan.php
@@ -1,5 +1,52 @@
 <?php 
 
 class M_karyawan {
+    private $primaryTable = 'karyawan';
+    private $joinTableJabatan = 'jabatan';
+    private $joinTableKategoriJabatan = 'kategori_jabatan';
+    private $db;
 
+    function __construct() {
+        $this->db = new Database();
+    }
+
+    //function for get all data karyawan from tabel karyawan in db
+    public function getAllKaryawan(){
+        $query = 'SELECT * from '.$this->primaryTable.' a 
+                LEFT JOIN '.$this->joinTableJabatan.' b 
+                ON a.id_jabatan = b.id
+                LEFT JOIN '.$this->joinTableKategoriJabatan.' c 
+                ON b.id_kategori_jabatan = c.id';
+        $this->db->query($query);
+        return $this->db->results();
+    }
+
+    //function for get data karyawan by id from tabel karyawan in db
+    public function getKaryawan($id){
+        $query = "SELECT * from ".$this->primaryTable." a 
+                LEFT JOIN ".$this->joinTableJabatan." b 
+                ON a.id_jabatan = b.id
+                LEFT JOIN ".$this->joinTableKategoriJabatan." c 
+                ON b.id_kategori_jabatan = c.id
+                WHERE a.id =:id";
+        $this->db->query($query);
+        $this->db->bind('id', $id);
+        return $this->db->result();
+    }
+
+    //function for get all data jabatan from tabel jabatan in db
+    public function getAllJabatan(){
+        $query = 'SELECT * from '.$this->joinTableJabatan.' a
+                LEFT JOIN '.$this->joinTableKategoriJabatan.' b
+                ON a.id_kategori_jabatan = b.id';
+        $this->db->query($query);
+        return $this->db->results();    
+    }
+
+    //function for get all data kategori jabatan from tabel kategori jabatan in db
+    public function getAllKategoriJabatan(){
+        $query = 'SELECT * from '.$this->joinTableKategoriJabatan;
+        $this->db->query($query);
+        return $this->db->results(); 
+    }
 }
