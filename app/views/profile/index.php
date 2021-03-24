a093166c43385423f6c1b4696f9158105c39acab
dmuktico <75906789+dmuktico27-05-98@users.noreply.github.com>
2021-03-24 21:17:06 +0700

    add view profile feature
    at views sound company profile

---
+++ app/views/profile/index.php
@@ -0,0 +1,107 @@
+<!-- Background Image -->
+<div class="bg-image">
+        <div class="h-100 bg-overlay">
+            <div class="h-100 w-100 container px-0">
+                <div class="h-100 w-100 d-flex justify-content-center align-items-center text-white">
+                    <header class="text-center">
+                        <h1 class="font-banner">
+                            Solusi untuk Hidup Mudah dan Terbantu, <br /> Integrasi Secepat Kilat !
+                        </h1>
+                    </header>
+                </div>
+              
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Section Berita Terpopuler -->
+ 
+        <div class="container px-0">
+            <div class="row mx-0">
+                <div class="row w-100 mx-0 padding-container">
+                    <p class="font-title"><br>Sound adalah produk teknologi baik lot, machine learning atau al dan masih banyak yang lainnya yang dinaungi oleh perusahaan atau pt sandi makmur teknologi.
+                    didirikan pada tahun 2021 dan memiliki bisnis b to b . sampai saat ini kami sedang explore dan terus mengembangkan teknologi kami dan pastinya kami open kepada perusahaan teknologi 
+                    lain untuk melakukan kerjasama dengan kami.</p>
+                    <p class="font-title font-weight-bold">Visi.</p>
+                    <p class="font-title">Menjadi sebuah perusahaan yang memiliki banyak manfaat bagi seluruh alam semesta dan memberikan solusi yang terbaik bagi mitra yang melakukan kerjasama dengan kami.</p>
+                    <p class="font-title font-weight-bold">Misi</p>
+                    <p class="font-title">.<br>- membuat teknologi yang canggih dan effisien.<br>- selalu memberikan service yang terbaik.<br>- berusaha menjaga kualitas produk.<br>
+                    - menyatukan beberapa perusahaan teknologi untuk saling kerjasama.<br>- membuat semuanya menjadi lebih baik.</p>
+                
+                </div>
+                <div class="row w-100 mx-0 ">
+
+                    <div class="col-md-12 padding-container py-3">
+                            <nav>
+                            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                                <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Managemen</a>
+                                <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Komisaris</a>
+                                <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Advisor</a>
+                            </div>
+                            </nav>
+                            <div class="tab-content" id="nav-tabContent">
+                            <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                           
+                                <div class="row w-100 mx-0 ">
+
+                                    <div class="col-md-3 padding-container py-3">
+                                        <div class="card">
+                                            <img class="card-img-top" src="<?= BASEURL ?>assets/image/default.jpg" alt="Card image cap">
+                                            <div class="card-body">
+                                                <h5 class="card-title">CEO</h5>
+                                                <p style="font-size: small; color: red;">Muhammad Irfan Dandung</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3 padding-container py-3">
+                                        <div class="card">
+                                            <img class="card-img-top" src="<?= BASEURL ?>assets/image/default.jpg" alt="Card image cap">
+                                            <div class="card-body">
+                                            <h5 class="card-title">CTO</h5>
+                                                <p style="font-size: small; color: red;">Dalu Mukti Pambagyo</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3 padding-container py-3">
+                                        <div class="card">
+                                            <img class="card-img-top" src="<?= BASEURL ?>assets/image/default.jpg" alt="Card image cap">
+                                            <div class="card-body">
+                                            <h5 class="card-title">CPO</h5>
+                                                <p style="font-size: small; color: red;">Cutyanti</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3 padding-container py-3">
+                                        <div class="card">
+                                            <img class="card-img-top" src="<?= BASEURL ?>assets/image/default.jpg" alt="Card image cap">
+                                            <div class="card-body">
+                                            <h5 class="card-title">COO</h5>
+                                                <p style="font-size: small; color: red;">Mila Hanif Tirta Yasa</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                 </div> 
+
+                            </div>
+                            <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                                fuasdhfhasifawejrhaufhjiasdhfihawejif
+                            </div>
+                            <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+                               asdfjiawefhuiawejfjkhajskdfhawnefokasjdfn
+                            </div>
+                            </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+
+    <!-- Close Tag Section Promo -->
+
+    
