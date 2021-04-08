d03110823bfb4602df62abcea7bb7ec9f7cf6997
irvandandung <irvandandung1@gmail.com>
2021-04-08 20:40:26 +0700

    update all

--- app/controllers/Berita.php
+++ app/controllers/Berita.php
@@ -5,8 +5,21 @@ class Berita extends Controller{
         $data['activePage'] = 'berita';
         $data['isCollapse'] = false;
         $data['allberita'] = $this->model('M_berita')->getAllBerita();
+        foreach($data['allberita'] as $key => $value){
+            $data['allberita'][$key]['tanggal'] = $this->indo_date($value['tanggal']);
+        }
         $this->view('template/header',$data);
         $this->view('berita/index', $data);
+        $this->view('template/footer');
+    }
+
+    public function detail($id){
+        $data['activePage'] = 'berita';
+        $data['isCollapse'] = false;
+        $data['berita'] = $this->model('M_berita')->getBerita($id);
+        $data['berita']['tanggal'] = $this->indo_date($data['berita']['tanggal']);
+        $this->view('template/header',$data);
+        $this->view('berita/detail', $data);
         $this->view('template/footer');
     }
 }
--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,6 +4,9 @@ class Home extends Controller {
     public function index(){
         $data['activePage'] = 'home';
         $data['upberita'] = $this->model('M_berita')->getAllBerita();
+        foreach($data['upberita'] as $key => $value){
+            $data['upberita'][$key]['tanggal'] = $this->indo_date($value['tanggal']);
+        }
         $this->view('template/header', $data);
         $this->view('home/index', $data);
         $this->view('template/footer');
