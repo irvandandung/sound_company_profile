7de6727baa909d83ff7f46ebf9b2ef2586130da6
irvandandung <irvandandung1@gmail.com>
2021-04-08 20:37:25 +0700

    delete print_r

--- app/core/App.php
+++ app/core/App.php
@@ -26,7 +26,6 @@ class App {
         //params
         if( !empty($url)){
             $this->params = array_values($url);
-            var_dump($url);
         }
 
         //run class & function or method
