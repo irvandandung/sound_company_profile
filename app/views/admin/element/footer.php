c0f4a7c33c29d2adf19a47e527b903742637a018
dmuktico <75906789+dmuktico27-05-98@users.noreply.github.com>
2021-04-19 13:16:46 +0700

    create footer.php

---
+++ app/views/admin/element/footer.php
@@ -0,0 +1,34 @@
+ <!--  content-wrapper ends -->
+ </div>
+ 
+          <footer class="footer">
+            <div class="container-fluid clearfix">
+              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © sound_id.site 2021</span>
+              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Web <a href="https://www.sound_id.site" target="_blank"></a>Developer</span>
+            </div>
+          </footer>
+          </div>
+          <!-- partial -->
+        
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="<?=base_url('admin_assets/vendors/js/vendor.bundle.base.js')?>"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="<?=base_url('admin_assets/vendors/chart.js/Chart.min.js')?>"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="<?=base_url('admin_assets/js/off-canvas.js')?>"></script>
+    <script src="<?=base_url('admin_assets/js/hoverable-collapse.js')?>"></script>
+    <script src="<?=base_url('admin_assets/js/misc.js')?>"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="<?=base_url('admin_assets/js/dashboard.js')?>"></script>
+    <script src="<?=base_url('admin_assets/js/todolist.js')?>"></script>
+
+    <!-- End custom js for this page -->
+
+  
+
+  </body>
+</html>
